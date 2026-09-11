965c3a47a396126c848aad6a99163b5c874a9dca
madzelsakre-ship-it <madzelsakre@gmail.com>
2026-09-11 20:30:00 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Login from './pages/Login';
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
